fb460096c3957c3a8154fdcc205ad3279511d05a
rensramos <rensramos17@gmail.com>
2016-07-18 18:32:16 +0800

    Update rsvp function

--- js/home.js
+++ js/home.js
@@ -7,7 +7,24 @@ main.init = function(){
     main.onMenuClick();
     main.onClickBrandLogo();
     main.onClickIntroButton();
+    main.onFormSubmit();
+}
 
+main.onFormSubmit = function (){
+    $("#form-content").submit(function(){
+       $("#ss-submit").attr("disabled", true);
+        setTimeout(function(){
+            $("#ss-submit").attr("disabled", false);
+        }, 1000);
+    });
+
+    $("#hidden_iframe").on('load',function () {
+        $("#myModal").modal('show');
+        $("#form-content")[0].reset();
+        setTimeout(function(){
+            $("#myModal").modal('hide');
+        }, 2000);
+    });
 }
 
 main.onClickIntroButton = function(){
